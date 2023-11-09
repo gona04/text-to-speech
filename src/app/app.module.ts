0fda14b2874d8e13cf60c127f366203d5617c2c9
Goonja <gandhi.goonja@gmail.com>
2023-11-09 16:47:33 -0500

    stashing for upgrading angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,62 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { VoiceRecorderComponent } from './voice-recorder/voice-recorder.component';
+import { MsalGuard, MsalInterceptor, MsalModule, MsalRedirectComponent } from '@azure/msal-angular';
+import { InteractionType, PublicClientApplication } from '@azure/msal-browser';
+import { HeaderComponent } from './header/header.component';
+import { AzureAdDemoService } from './azure-ad-demo.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    VoiceRecorderComponent
+    VoiceRecorderComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MsalModule.forRoot(new PublicClientApplication
+      (
+        {
+          auth: {
+            clientId: "eea97fa2-b2fe-4d4e-9ad1-4c7f96b755cc",
+            redirectUri: "http://localhost:4200",
+            authority:"https://login.microsoftonline.com/6ba0b651-eb6c-40de-90bf-b1e0434967f6"
+          },
+          cache: {
+            cacheLocation: 'localStorage',
+            storeAuthStateInCookie: false
+          }
+        }
+      ),
+      {
+        interactionType: InteractionType.Redirect,
+        authRequest: {
+          scopes: ['user.read']
+        }
+      },
+      {
+        interactionType: InteractionType.Redirect,
+        protectedResourceMap: new Map(
+          [
+            ['https://graph.microsoft.com/v1.0/me', ['user.Read']]
+          ]
+        )
+      }
+      )
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+    provide: HTTP_INTERCEPTORS,
+    useClass: MsalInterceptor,
+    multi:true,
+  }, MsalGuard, AzureAdDemoService
+],
+  bootstrap: [AppComponent,
+     MsalRedirectComponent
+    ]
 })
 export class AppModule { }
