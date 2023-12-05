2df5309476d960c8c65d800263cbd46985192180
Goonja <gandhi.goonja@gmail.com>
2023-12-05 11:17:16 -0500

    created shared libraries

---
+++ projects/my-shared-lib/src/lib/my-shared-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-my-shared-lib',
+  template: `
+    <p>
+      my-shared-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class MySharedLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
