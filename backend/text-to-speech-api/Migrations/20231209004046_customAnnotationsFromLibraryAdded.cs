8154d1d094d10626df0d5ba7536d483fdf28106b
Goonja <gandhi.goonja@gmail.com>
2023-12-09 00:51:54 +0000

    created an library with data annotaions and is working fine

---
+++ backend/text-to-speech-api/Migrations/20231209004046_customAnnotationsFromLibraryAdded.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace texttospeechapi.Migrations
+{
+    /// <inheritdoc />
+    public partial class customAnnotationsFromLibraryAdded : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "ConvertedTextItem",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(100)",
+                oldMaxLength: 100,
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "ConvertedTextItem",
+                type: "varchar(100)",
+                maxLength: 100,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
--- backend/text-to-speech-api/Models/ConvertedTextItem.cs
+++ backend/text-to-speech-api/Models/ConvertedTextItem.cs
@@ -1,20 +1,20 @@
 using System;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using CustomAnnotations;
 
 namespace text_to_speech_api.Models
 {
     public class ConvertedTextItem
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [System.ComponentModel.DataAnnotations.Schema.DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
 
         [Required(ErrorMessage = "Text is required.")]
         [MaxLength(5000, ErrorMessage = "Text cannot exceed 5000 characters.")]
         public string? Text { get; set; }
 
-        [MaxLength(100, ErrorMessage = "Username cannot exceed 100 characters.")]
+        [CustomAnnotations.CustomValidationAttribute(maxLength: 100, isRequired: true, dataType: DataType.Text)]
         public string? UserName { get; set; }
 
         [Required(ErrorMessage = "CreatedAt is required.")]
