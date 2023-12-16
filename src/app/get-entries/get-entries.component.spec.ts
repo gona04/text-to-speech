4389065cf8e2064a3de896e199b379e6e4c93f3d
Goonja <gandhi.goonja@gmail.com>
2023-12-15 23:31:14 -0500

    test cases for modal added

--- src/app/get-entries/get-entries.component.spec.ts
+++ src/app/get-entries/get-entries.component.spec.ts
@@ -1,47 +1,84 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { GetEntriesComponent } from './get-entries.component';
 import { DairyEntriesService } from '../dairy-entries.service';
-import { of } from 'rxjs';
+import { AudioLibraryService } from 'projects/audio-library/src/public-api';
+import { Router } from '@angular/router';
 import { DairyEntries } from '../models/diaryEntries';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { HttpClientModule } from '@angular/common/http';
 
 describe('GetEntriesComponent', () => {
   let component: GetEntriesComponent;
   let fixture: ComponentFixture<GetEntriesComponent>;
-  let dairyEntriesService: jasmine.SpyObj<DairyEntriesService>;
-
-  const response: DairyEntries[] = [
-    { id: 1, text: 'Sample text', userName: 'JohnDoe', createdAt: '2023-01-01T12:34:56', createdAtTime: '12:34:56' }
-  ];
+  let dairyEntriesServiceSpy: jasmine.SpyObj<DairyEntriesService>;
+  let audioLibraryServiceSpy: jasmine.SpyObj<AudioLibraryService>;
+  let routerSpy: jasmine.SpyObj<Router>;
 
   beforeEach(() => {
-    const spy = jasmine.createSpyObj('DairyEntriesService', ['getDairyEntries']);
+    dairyEntriesServiceSpy = jasmine.createSpyObj('DairyEntriesService', ['getDairyEntries', 'getCompleteAnalysis']);
+    audioLibraryServiceSpy = jasmine.createSpyObj('AudioLibraryService', ['sendDataTobeEdited']);
+    routerSpy = jasmine.createSpyObj('Router', ['navigate']);
 
     TestBed.configureTestingModule({
       declarations: [GetEntriesComponent],
-      providers: [{ provide: DairyEntriesService, useValue: spy }],
-      imports: [HttpClientTestingModule, HttpClientModule],
+      providers: [
+        { provide: DairyEntriesService, useValue: dairyEntriesServiceSpy },
+        { provide: AudioLibraryService, useValue: audioLibraryServiceSpy },
+        { provide: Router, useValue: routerSpy }
+      ]
     });
 
     fixture = TestBed.createComponent(GetEntriesComponent);
     component = fixture.componentInstance;
-    dairyEntriesService = TestBed.inject(DairyEntriesService) as jasmine.SpyObj<DairyEntriesService>;
   });
 
-  it('should create the component', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should get all entries on ngOnInit', () => {
-    // Arrange
-    dairyEntriesService.getDairyEntries.and.returnValue(of(response));
+  it('should fetch all entries on initialization', () => {
+    const mockEntries: DairyEntries[] = [{
+      id: 0,
+      text: 'Hello my day was okay',
+      userName: 'Goonja',
+      createdAt: new Date().toString(),
+      createdAtTime: new Date().toString()
+     }];
+    dairyEntriesServiceSpy.getDairyEntries.and.returnValue(of(mockEntries));
 
-    // Act
     component.ngOnInit();
 
-    // Assert
-    expect(dairyEntriesService.getDairyEntries).toHaveBeenCalled();
-    expect(component.diaryEntries).toEqual(response);
+    expect(component.diaryEntries).toEqual(mockEntries);
+  });
+
+  it('should format date and time for each entry', () => {
+    const mockEntries: DairyEntries[] = [{ id: 0, text: 'what is the time ?', userName: 'Goonja'. createdAt: new Date() }];
+    dairyEntriesServiceSpy.getDairyEntries.and.returnValue(of(mockEntries));
+
+    component.ngOnInit();
+
+    const formattedDate = component.diaryEntries[0].createdAt;
+    const formattedTime = component.diaryEntries[0].createdAtTime;
+
+    expect(formattedDate).toBeDefined();
+    expect(formattedTime).toBeDefined();
+  });
+
+  it('should call getCompleteAnalysis and set analysis modal view on getTheAnalysis', () => {
+    const mockAnalysisResult = { /* your mock analysis data */ };
+    dairyEntriesServiceSpy.getCompleteAnalysis.and.returnValue(of(mockAnalysisResult));
+
+    component.getTheAnalysis();
+
+    expect(dairyEntriesServiceSpy.getCompleteAnalysis).toHaveBeenCalledOnceWith();
+    expect(audioLibraryServiceSpy.setAnalysisModalView).toHaveBeenCalledOnceWith(true, mockAnalysisResult);
+  });
+
+  it('should navigate to home and send data to be edited on getDataThatNeedsTobeEdited', () => {
+    const mockData = { /* your mock data */ };
+
+    component.getDataThatNeedsTobeEdited(mockData);
+
+    expect(audioLibraryServiceSpy.sendDataTobeEdited).toHaveBeenCalledOnceWith(mockData);
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['home']);
   });
 });
--- src/app/modal-complete-analysis/modal-complete-analysis.component.spec.ts
+++ src/app/modal-complete-analysis/modal-complete-analysis.component.spec.ts
@@ -1,23 +1,47 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ModalCompleteAnalysisComponent } from './modal-complete-analysis.component';
+import { DairyEntriesService } from '../dairy-entries.service';
+import { of } from 'rxjs';
 
 describe('ModalCompleteAnalysisComponent', () => {
   let component: ModalCompleteAnalysisComponent;
   let fixture: ComponentFixture<ModalCompleteAnalysisComponent>;
+  let dairyEntriesServiceSpy: jasmine.SpyObj<DairyEntriesService>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ModalCompleteAnalysisComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    dairyEntriesServiceSpy = jasmine.createSpyObj('DairyEntriesService', ['getAnalysisData', 'setAnalysisModalView']);
+
+    TestBed.configureTestingModule({
+      declarations: [ModalCompleteAnalysisComponent],
+      providers: [{ provide: DairyEntriesService, useValue: dairyEntriesServiceSpy }]
+    });
 
     fixture = TestBed.createComponent(ModalCompleteAnalysisComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call getAnalysisData and set data on ngOnInit', () => {
+    const mockAnalysisData = { /* your mock analysis data */ };
+    dairyEntriesServiceSpy.getAnalysisData.and.returnValue(of(mockAnalysisData));
+
+    component.ngOnInit();
+
+    expect(dairyEntriesServiceSpy.getAnalysisData).toHaveBeenCalledOnceWith();
+    expect(component.data).toEqual(mockAnalysisData);
+  });
+
+  it('should call print when printPage is called', () => {
+    spyOn(window, 'print');
+    component.printPage();
+    expect(window.print).toHaveBeenCalledOnceWith();
+  });
+
+  it('should call setAnalysisModalView with false on close', () => {
+    component.close();
+    expect(dairyEntriesServiceSpy.setAnalysisModalView).toHaveBeenCalledOnceWith(false);
   });
 });
