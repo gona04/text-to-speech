023797ea89562046fb3aae1b031fc38528853a25
Goonja <gandhi.goonja@gmail.com>
2023-12-15 23:21:48 -0500

    added customAnnotaions for sentiment analysis items

---
+++ backend-libraries/CustomAnnotationsSentimentAnalysis/CustomAnnotationsSentimentAnalysis.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CustomAnnotationsSentimentAnalysis
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = false)]
+    public class MaxLengthAndTypeValidationAttribute : ValidationAttribute
+    {
+        private readonly int _maxLength;
+        private readonly bool _isRequired;
+        private readonly System.ComponentModel.DataAnnotations.DataType _dataType;
+
+        public MaxLengthAndTypeValidationAttribute(int maxLength, bool isRequired, System.ComponentModel.DataAnnotations.DataType dataType)
+        {
+            _maxLength = maxLength;
+            _isRequired = isRequired;
+            _dataType = dataType;
+
+            ErrorMessage = $"The field cannot exceed {_maxLength} characters.";
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            // Custom validation logic
+            if (_isRequired && (value == null || string.IsNullOrWhiteSpace(value.ToString())))
+            {
+                return new ValidationResult("This field is required.");
+            }
+
+            if (value is string str && str.Length > _maxLength)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
--- backend/text-to-speech-api/Models/SentimentAnalyserItem.cs
+++ backend/text-to-speech-api/Models/SentimentAnalyserItem.cs
@@ -1,5 +1,12 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+
+// Add these using directives
+using System.ComponentModel;
+
+// SentimentAnalyserItem.cs
+using SentimentValidationAnnotations = CustomAnnotationsSentimentAnalysis;
 
 namespace text_to_speech_api.Models
 {
@@ -10,19 +17,23 @@ namespace text_to_speech_api.Models
         public long Id { get; set; }
 
         [Range(0, 100, ErrorMessage = "Value must be between 0 and 100.")]
+        [SentimentValidationAnnotations.MaxLengthAndTypeValidationAttribute(100, true, DataType.Text)] // Use DataType.Text for int
         public int Positive { get; set; }
 
         [Range(0, 100, ErrorMessage = "Value must be between 0 and 100.")]
+        [SentimentValidationAnnotations.MaxLengthAndTypeValidationAttribute(100, true, DataType.Text)] // Use DataType.Text for int
         public int Negative { get; set; }
 
         [Range(0, 100, ErrorMessage = "Value must be between 0 and 100.")]
+        [SentimentValidationAnnotations.MaxLengthAndTypeValidationAttribute(100, true, DataType.Text)] // Use DataType.Text for int
         public int Neutral { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Sentiment is required")]
+        [SentimentValidationAnnotations.MaxLengthAndTypeValidationAttribute(int.MaxValue, true, DataType.Text)]
         public string? sentiment { get; set; }
 
         [Required]
         [ForeignKey("ConvertedTextItem")]
         public long ConvertedTextId { get; set; }
-  }
+    }
 }
